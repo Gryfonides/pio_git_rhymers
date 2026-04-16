5ef2acfc6a973de690d63c0f56855fd899dbf593
Kacper <245934@edu.p.lodz.pl>
2026-04-16 15:19:12 +0200

    Zad 9-12 hermetyzacja, getter

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -4,18 +4,18 @@ public class DefaultCountingOutRhymer {
 
     private int[] NUMBERS = new int[12];
 
-    public int total = -1;
+    private int total = -1;
 
-    public void countIn(int in) {
+    protected void countIn(int in) {
         if (!isFull())
             NUMBERS[++total] = in;
     }
 
-    public boolean callCheck() {
+    protected boolean callCheck() {
         return total == -1;
     }
 
-    public boolean isFull() {
+    protected boolean isFull() {
         return total == 11;
     }
 
@@ -25,10 +25,13 @@ public class DefaultCountingOutRhymer {
         return NUMBERS[total];
     }
 
-    public int countOut() {
+    protected int countOut() {
         if (callCheck())
             return -1;
         return NUMBERS[total--];
     }
 
+    public int getTotal() {
+        return total;
+    }
 }
